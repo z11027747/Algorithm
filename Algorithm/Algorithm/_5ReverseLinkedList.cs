ec7cb071d4d2fb4e1232b33d48e275b8074e5728
APP4577 <11027747@qq.com>
2021-12-09 00:04:08 +0800

    链表反转

---
+++ Algorithm/Algorithm/_5ReverseLinkedList.cs
@@ -0,0 +1,40 @@
+﻿namespace Algorithm
+{
+    public class _5ReverseLinkedList
+    {
+        public class LinkListNode
+        {
+            public int value;
+            public LinkListNode next;
+        }
+
+        public static void Iterate()
+        {
+            LinkListNode head = new LinkListNode();
+
+            LinkListNode prev = null;
+            LinkListNode curr = head;
+
+            while (curr != null)
+            {
+                LinkListNode next = curr.next;
+                curr.next = prev;
+
+                prev = curr;
+                curr = next;
+            }
+        }
+
+        public static LinkListNode Recursion(LinkListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            LinkListNode newHead = Recursion(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newHead;
+        }
+    }
+}
