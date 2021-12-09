c8fb0c529e748d5b48111f96e13c13ba45b536a5
APP4577 <11027747@qq.com>
2021-12-10 00:00:34 +0800

    寻找数组中心下标

---
+++ Algorithm/Algorithm/_7PivotIndex.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Algorithm
+{
+    public class _7PivotIndex
+    {
+        public static int PivotIndex(int[] nums)
+        {
+            int sum = nums.Sum(item => item);
+
+            int total = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total += nums[i];
+
+                if (total == sum)
+                {
+                    return i;
+                }
+
+                sum -= nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
