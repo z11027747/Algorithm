bfc053ac033bd5481c22cab123a4c263ae223ee1
APP4577 <11027747@qq.com>
2021-12-10 00:00:09 +0800

    删除重复项             return new int[0];

---
+++ Algorithm/Algorithm/_6RemoveDuplicates.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithm
+{
+    public class _6RemoveDuplicates
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int i = 0;
+            int j = 1;
+
+            while (j < nums.Length)
+            {
+                if (nums[i] != nums[j])
+                {
+                    i++;
+                    j++;
+                    nums[i + 1] = nums[j];
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return i;
+        }
+    }
+}
