59097205790f7b64e7b9b152d172706ec1a5bf13
APP4577 <11027747@qq.com>
2021-12-12 22:45:15 +0800

    子数组最大平均数

---
+++ Algorithm/Algorithm/__15AvgArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algorithm
+{
+    public class __15AvgArray
+    {
+        public static double FindMaxAverage(int[] nums, int k)
+        {
+            int sum = 0;
+            int n = nums.Length;
+
+            //统计第一个窗口的和
+            for (int i = 0; i < k; i++)
+            {
+                sum += nums[i];
+            }
+
+            int max = sum;
+
+            for (int i = k; i < n; i++)
+            {
+                sum -= nums[i - k];
+                sum += nums[i];
+
+                max = Math.Max(sum, max);
+            }
+
+            return max * 1.0 / k;
+        }
+    }
+}
