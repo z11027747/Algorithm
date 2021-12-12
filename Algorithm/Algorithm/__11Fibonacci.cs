64be0736f2636f3e889fd2ca5524ad84c0029fef
APP4577 <11027747@qq.com>
2021-12-12 22:43:48 +0800

    斐波那契

---
+++ Algorithm/Algorithm/__11Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿namespace Algorithm
+{
+    public class __11Fibonacci
+    {
+        public static int Bf(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return Bf(n - 1) + Bf(n - 2);
+        }
+
+        public static int Recurse(int[] arr, int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (arr[n] != 0)
+            {
+                return arr[n];
+            }
+
+            arr[n] = Bf(n - 1) + Bf(n - 2);
+
+            return arr[n];
+        }
+    }
+}
